c11da364f40bf838aa9af31e016b1500f007951c
ManuRodriguezC <estebanclimb@gmail.com>
2025-06-30 19:37:40 -0500

    Check and fixed error for deploy

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, s as start } from './chunks/entrypoint_De3uo4N0.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_BV-CFC0l.mjs';
-export { renderers } from '../renderers.mjs';

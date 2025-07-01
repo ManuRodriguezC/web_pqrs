4539f5b9929b2bdfb0335a1aee21aa6f97dd5be4
ManuRodriguezC <estebanclimb@gmail.com>
2025-06-30 19:28:48 -0500

    Update for deploy in vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: "server", // Esto permite tener server y client.
